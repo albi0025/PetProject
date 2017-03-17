f41c48786f753c37cdcdef30aa560521b478e487
Jennifer Phelps <albi0025@gmail.com>
2017-03-16 23:09:36 -0600

    test setup

--- helpers/scrape.js
+++ helpers/scrape.js
@@ -1,10 +1,15 @@
 
+let request = require('request');
 let scraper = {};
 
 
 scraper.scrapePetango = function(url) {
   //Make a GET request
-
+  request(url, function (error, response, body) {
+    // console.log('error:', error); // Print the error if one occurred
+    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+  });
   //Parse the response and return and array of Urls to the animals
   let animalUrls = this.parseAnimalListResponse("");
 
@@ -19,7 +24,10 @@ scraper.scrapePetango = function(url) {
   return petArray;
 };
 
+//This function takes a response which it an HTML string
+//and returns and array of url strings.
 scraper.parseAnimalListResponse = function(response) {
+  console.log(response);
   return [];
 };
 
