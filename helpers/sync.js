ef427a5b3ef5c8dc5a2885d88f26555ad8bf331a
Jennifer Phelps <albi0025@gmail.com>
2017-03-20 10:23:58 -0600

    clean up

---
+++ helpers/sync.js
@@ -0,0 +1,6 @@
+//You scrape petango and you get one pet object back.
+//You want to look that pet up in your database by animalId.
+//If the pet does not exsist you want to insert and save in database.
+//If the pet does not exsist in the scrape but exsists in the DB we want to
+//remove or hide the pet.
+//If the the pet exsits in both the scrape and the database, update data.
