1579e304af27fe98f7f59cea5d33f959d5469478
Jennifer Phelps <albi0025@gmail.com>
2017-03-20 13:54:45 -0600

    pet model changes

--- helpers/sync.js
+++ helpers/sync.js
@@ -1,6 +1,33 @@
 //You scrape petango and you get one pet object back.
 //You want to look that pet up in your database by animalId.
-//If the pet does not exsist you want to insert and save in database.
-//If the pet does not exsist in the scrape but exsists in the DB we want to
-//remove or hide the pet.
-//If the the pet exsits in both the scrape and the database, update data.
+let Pet = require('../models/pets');
+let sync = {};
+
+sync.syncDatabase = function(arr) {
+
+//loop over pet objects in array
+  for(let i = 0; i < arr.length; i ++) {
+    //look up pet in database by animalID
+    Pet.find({pet.animalId}, function (err, docs) {
+      // docs is an array
+    });
+    //save pet object if it does not exist
+    //remove pet object if it only exists in the DB and not the scrape
+    console.log(arr[i]);
+  }
+//save pets that have an ID that does not currently exsist in the database
+//remove/hide pets that exist in the database but do not exist
+};
+
+sync.saveNewPets = function(id) {
+
+};
+
+sync.removeAdoptedPets = function(id) {
+
+};
+
+
+
+
+module.exports = sync;
