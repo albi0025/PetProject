0ad2732ec735eef9549d310e18e3a3011a2dcaec
Adam <adam.codework@gmail.com>
2017-03-24 14:08:18 -0600

    changed the Header, added some styling to the header and home page dog and cat div. added logo to header.

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,33 +7,35 @@ export default class Home extends React.Component {
   render() {
     return(
       <div>
-        <div className="header">
-          <h1 className="words">Welcome to Pet Project</h1>
-            <div className="headerimg">
+        <div className="container">
+          <div className="headerlogo">
+            <div className="header">
+              <h1 className="words">Welcome</h1>
+            </div>
           </div>
         </div>
         <Grid>
           <Row>
-           <Col xs={12} md={6} lg={6}>
-             <Thumbnail className="homethumb" src="../images/mainpuppy.jpg" alt="242x200">
-               <h1><Link to={{ pathname: '/DisplayPets', query: { species: 'dog' } }}>Dogs</Link></h1>
-             </Thumbnail>
-           </Col>
-           <Col xs={12} md={6} lg={6}>
-             <Thumbnail src="../images/maincat.jpg" alt="242x200">
-               <h1><Link to={{ pathname: '/DisplayPets', query: { species: 'cat' } }}>Cats</Link></h1>
-             </Thumbnail>
-           </Col>
+            <Col xs={12} md={6} lg={6}>
+              <div className="outterhomediv">
+                <Thumbnail className="homediv" src="../images/mainpuppy.jpg" alt="242x200">
+                  <h1><Link to={{ pathname: '/DisplayPets', query: { species: 'dog' } }}>Dogs</Link></h1>
+                </Thumbnail>
+              </div>
+            </Col>
+            <Col xs={12} md={6} lg={6}>
+              <div className="outterhomediv">
+                <Thumbnail className="homediv" src="../images/maincat.jpg" alt="242x200">
+                  <h1><Link to={{ pathname: '/DisplayPets', query: { species: 'cat' } }}>Cats</Link></h1>
+                </Thumbnail>
+              </div>
+            </Col>
           </Row>
         </Grid>
-        <Grid>
-          <Row>
-            <div>
-              <h1>Sponsorship</h1>
-              <p></p>
-            </div>
-          </Row>
-        </Grid>
+          <div className="sponsorship">
+            <h1 className="sponsorhead">Sponsorship</h1>
+            <p></p>
+          </div>
       </div>
     );
   }
