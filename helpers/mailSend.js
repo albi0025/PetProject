5160cd49e55ed3f48475e8bcb925a692ab2683a8
3x0pandemic <timwalsh237@gmail.com>
2017-04-03 09:30:39 -0600

    Tim's style

--- helpers/mailSend.js
+++ helpers/mailSend.js
@@ -13,8 +13,7 @@ export default function petAddedEmail(newPets, recipients) {
     to: 'timwalsh237@gmail.com',
     bcc: recipients,
     subject: newPetCount + ' Pets Have Been Added!',
-    text: newPetCount + ' Pets have been added to the Bozeman Pet Project! Please visit https://bozeman-pet-project.herokuapp.com/ to view the new additions and find your new best friend!',
-    html: '<div style="width: 500px; height: 400px: background: #ebebeb; color: #ddd"><p>Hi  + "recipients.name" + \n ,this email is to inform you that has added their bio to the knowledge Base \n</p></div>'
+    text: newPetCount + ' Hello, Pets have been added to the Bozeman Pet Project! Please visit https://bozeman-pet-project.herokuapp.com/ to view the new additions and find your new best friend!'
   };
 
   mailgun.messages().send(data, function (error, body) {
