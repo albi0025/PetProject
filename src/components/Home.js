75a95f2f830de7303b3601a68fa36a23df3890a9
Adam <adam.codework@gmail.com>
2017-04-04 10:32:23 -0600

    got the display pets page to be responsive, added some more styling to the navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,23 +9,31 @@ export default class Home extends React.Component {
       <div>
         <div className="homeContainer">
           <div className="header">
-            <h1 className="words">Welcome to The</h1>
+            <h1 className="words"/>
           </div>
         </div>
         <Grid>
           <Row>
-            <Col xs={12} md={6} lg={6}>
-            <Link to={{pathname: '/DogDisplay'}}>
-              <Thumbnail className="homediv1" src="../images/mainpuppy.jpg" alt="242x200"/>
-            </Link>
-            </Col>
-            <Col xs={12} md={6} lg={6}>
-            <Link to={{pathname: '/CatDisplay'}}>
-              <Thumbnail className="homediv2" src="../images/maincat.jpg" alt="242x200"/>
-              </Link>
-            </Col>
+            <div className="homepetcontainer">
+              <Col xs={12} md={6} lg={6}>
+                <Link to={{pathname: '/DogDisplay'}}>
+                  <Thumbnail className="homediv1" src="../images/mainpuppy.jpg" alt="242x200"/>
+                </Link>
+              </Col>
+              <Col xs={12} md={6} lg={6}>
+                <Link to={{pathname: '/CatDisplay'}}>
+                  <Thumbnail className="homediv2" src="../images/maincat.jpg" alt="242x200"/>
+                </Link>
+              </Col>
+            </div>
           </Row>
         </Grid>
+        <div className="sponsorship">
+          <h1 className="sponsorshipHeader">Sponsorship</h1>
+          <p>It takes between $250 to $400 to care for one animal in our shelter for a year.
+          with a little help from you our fuzzy four legged friends can stay warm and fed.</p>
+          <p>sponsor an animal today!</p>
+        </div>
       </div>
     );
   }
